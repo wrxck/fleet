ae709015ba152199d6ba3950e2b2aac8579f1358
Matt Hesketh <matt@heskethwebdesign.co.uk>
2026-04-12 21:09:21 +0000

    feat(bot): add adapter interface and message types

---
+++ bot/adapter/adapter.go
@@ -0,0 +1,50 @@
+package adapter
+
+import "context"
+
+// InboundMessage represents a message received from any messaging provider.
+type InboundMessage struct {
+	ChatID    string
+	SenderID  string
+	Text      string
+	HasPhoto  bool
+	PhotoData []byte
+	Provider  string
+}
+
+// OutboundMessage represents a message to be sent via any messaging provider.
+type OutboundMessage struct {
+	Text     string
+	Photo    []byte
+	Document []byte
+	Caption  string
+	Options  []string
+}
+
+// TextResponse constructs an OutboundMessage containing only text.
+func TextResponse(text string) OutboundMessage {
+	return OutboundMessage{Text: text}
+}
+
+// OptionsResponse constructs an OutboundMessage with text and a list of reply options.
+func OptionsResponse(text string, options []string) OutboundMessage {
+	return OutboundMessage{Text: text, Options: options}
+}
+
+// Adapter is the interface that all messaging provider adapters must implement.
+type Adapter interface {
+	// Name returns the identifier for this adapter (e.g. "telegram", "imessage").
+	Name() string
+
+	// Start begins receiving messages, sending inbound messages to the inbox channel.
+	Start(ctx context.Context, inbox chan<- InboundMessage) error
+
+	// Send delivers a message to the given chat ID.
+	Send(chatID string, msg OutboundMessage) error
+
+	// SendAlert delivers an alert message to the configured alert destination.
+	SendAlert(text string) error
+
+	// Stop gracefully shuts down the adapter.
+	Stop() error
+}
