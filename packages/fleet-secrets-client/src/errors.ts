96df63610b185743c93dbb95e9759b912bf70fb3
Matt Hesketh <matt@heskethwebdesign.co.uk>
2026-05-16 18:32:27 +0000

    chore: empty tree as base for full-codebase review

--- packages/fleet-secrets-client/src/errors.ts
+++
@@ -1,8 +0,0 @@
-export class FleetSecretsError extends Error {
-  readonly code: string;
-  constructor(message: string, code: string = 'fleet_secrets_error') {
-    super(message);
-    this.name = 'FleetSecretsError';
-    this.code = code;
-  }
-}
